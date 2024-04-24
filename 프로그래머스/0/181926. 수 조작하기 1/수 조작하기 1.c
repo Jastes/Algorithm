edc9acab838eafe41edc0139922ed7e01719481d
최준성 <53941330+Jastes@users.noreply.github.com>
2024-04-24 14:06:29 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.15 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
@@ -5,14 +5,21 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(int n, const char* control) {
-    int answer = n;
+    char* ptr = control - 1;
     
-    for(int i=0; i<strlen(control); i++){
-        if(control[i] == 'w')   answer += 1;
-        else if(control[i] == 's')  answer -= 1;
-        else if(control[i] == 'd')  answer += 10;
-        else if(control[i] == 'a')  answer -= 10;
+    while(*++ptr){
+        if(*ptr == 'w')   n += 1;
+        else if(*ptr == 's')  n -= 1;
+        else if(*ptr == 'd')  n += 10;
+        else if(*ptr == 'a')  n -= 10;
     }
     
-    return answer;
+    // for(int i=0; i<strlen(control); i++){
+    //     if(control[i] == 'w')   answer += 1;
+    //     else if(control[i] == 's')  answer -= 1;
+    //     else if(control[i] == 'd')  answer += 10;
+    //     else if(control[i] == 'a')  answer -= 10;
+    // }
+    
+    return n;
 }
