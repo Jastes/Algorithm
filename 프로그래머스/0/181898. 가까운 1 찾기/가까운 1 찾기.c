c7c1adc22caf59b413517326493aa47de0c32b0c
최준성 <53941330+Jastes@users.noreply.github.com>
2024-07-04 14:12:09 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.c
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.c
@@ -6,7 +6,7 @@
 int solution(int arr[], size_t arr_len, int idx) {
     int answer = -1;
     
-    for(size_t i=idx+1; i < arr_len; i++){
+    for(size_t i=idx+1; i < arr_len; i++){ // 문제가 뭔가 이상하다..?
         if(arr[i] == 1){
             answer = i;
             break;
